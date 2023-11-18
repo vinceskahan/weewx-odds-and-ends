f764c31447d60af151b4a3da2c1aa0bb9c0114d0
Vince Skahan <vinceskahan@gmail.com>
2023-11-18 14:12:36 -0800

    log to stdout

--- weetest.sh
+++ weetest.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 #
-# automate running the weewx benchmark from the wiki
-#   https://github.com/weewx/weewx/wiki/Benchmarks-of-file-and-image-generation
+# automate running the weewx benchmarks
+#   ref: https://github.com/weewx/weewx/wiki/Benchmarks-of-file-and-image-generation
 #
 
 # location of benchmark
@@ -13,10 +13,7 @@ TEST_DIR="/var/tmp/weetest"
 # location the benchmark will extract to
 BENCHMARK_DIR="/var/tmp/weewx-benchmark"
 
-
 #---- stop editing here ---
-#
-# send pip STDERR to /dev/null due to deprecation warnings
 #
 # we also send weectl and wee_reports output to /dev/null
 # because there is no --quiet option to those commands
@@ -27,19 +24,32 @@ cd ${TEST_DIR}
 python3 -m venv weetest-venv
 source weetest-venv/bin/activate
 pip3 install weewx --quiet 2>/dev/null
-weectl station create --config=${TEST_DIR}/weetest-data/weewx.conf --no-prompt >/dev/null
+#weectl station create --config=${TEST_DIR}/weetest-data/weewx.conf --no-prompt >/dev/null
 
 echo "... downloading and extracting benchmark code ..."
-wget -qO- ${BENCHMARK_DIR} | tar xz -C /var/tmp
+wget -qO- ${BENCHMARK_TGZ} | tar xz -C /var/tmp
 
 if [ -d ${BENCHMARK_DIR}/public_html ]
 then
   echo "... cleaning up previous benchmark runs ..."
-  rm -r ${BENCHMARK_TGZ}/public_html
+  rm -r ${BENCHMARK_DIR}/public_html
 fi
 
+echo "... appending stdout logging to weewx.conf ..."
+cat >>${BENCHMARK_DIR}/weewx.conf <<-EOF
+[Logging]
+    version = 1
+    disable_existing_loggers = True
+
+    [[loggers]]
+        [[[weewx.cheetahgenerator]]]
+          handlers = console,
+        [[[weewx.imagegenerator]]]
+          handlers = console,
+EOF
+
 echo "... running the weewx benchmark ..."
-wee_reports ${BENCHMARK_DIR}/weewx.conf >/dev/null
+wee_reports ${BENCHMARK_DIR}/weewx.conf
 
 echo "... deactivating python venv ..."
 deactivate
@@ -50,14 +60,5 @@ echo ""
 echo "To remove the test-use weewx, benchmark code, and data:"
 echo "   rm -r ${TEST_DIR}"
 echo "   rm -r ${BENCHMARK_DIR}"
-echo ""
-
-# this requires rsyslog installed and running...
-echo ""
-echo "If you have rsyslog installed"
-echo "to see your results:"
-echo "  grep wee_reports /var/log/syslog"
-echo ""
-echo "(if you have rsyslog enabled)"
 echo ""
 
