08bfe3f7e13a9de3441124272e20b944b421969e
Vince Skahan <vinceskahan@gmail.com>
2024-06-21 14:43:53 -0700

    update and simplify a little

--- weewx-backup-copy.sh
+++ weewx-backup-copy.sh
@@ -1,52 +1,60 @@
 #!/bin/bash
 #
 # weewx backup (originally based on wview)
+#
+# this backs up your sqlite3 database(s) safely by working
+# off a saved copy of each.  You will need to do a little
+# light editing below to set your filenames and directories
+# you want to work in.  It's up to you to periodically prune
+# old saved copies as well as stashing the .gz files to a
+# different computer just-in-case.   FWIW, I rsync the
+# ${DESTDIR} below to another computer nightly via cron.
+#
+
+#----------- START EDITING HERE ------------------------
+
+# files to back up
+FILE_LIST="vp2.sdb mem.sdb purpleair.sdb ecowitt.sdb"
+
+# location of weewx database(s)
+ARCHIVE_DIR=/home/pi/weewx-data/archive
+
+# where to back up to
+DESTDIR=/home/pi/weewx-backups
+
+# scratch dir to work in
+TMPDIR="/var/tmp"
+
+#------------- STOP EDITING HERE -----------------------
 
 TODAY=`date +%Y_%m_%d`
 
-TMPDIR="/var/tmp"
+# processing every file, we:
+#    cd to the archive dir and copy to a tmpdir
+#    cd to the tmpdir and gzip the copy up
+#    move the gzipped copy to the destination dir
+#    clean up the temporary unzipped copy
 
-ARCHIVE_FILE=vp2.sdb
-MEM_FILE=mem.sdb
-PA_FILE=purpleair.sdb
+logger "WEEWX_BACKUP - starting"
+for f in ${FILE_LIST}
+do
+  cd ${ARCHIVE_DIR}
+  if [ -f ${f} ]
+  then
+     logger "WEEWX_BACKUP - backing up ${f}"
 
-# moved 2023-0413 to wee5 pip location
-ARCHIVE_DIR=/home/pi/weewx-data/archive
+     cp ${f} ${TMPDIR}
+     cd ${TMPDIR}
 
-DESTDIR=/home/pi/weewx-backups
+     gzip -c ${f} > ${f}.${TODAY}.gz
 
-if [ ! -f $ARCHIVE_DIR/$ARCHIVE_FILE ]; then
-	logger "$0 exiting - srcfile not found"
-	exit 1
-fi
+     mv ${f}.${TODAY}.gz ${DESTDIR}
 
-cd $ARCHIVE_DIR
-
-cp $ARCHIVE_FILE ${TMPDIR}
-cp $MEM_FILE     ${TMPDIR}
-cp $PA_FILE      ${TMPDIR}
-
-cd "${TMPDIR}"
-
-gzip -c ${MEM_FILE} > $MEM_FILE.$TODAY.gz
-mv $MEM_FILE.$TODAY.gz $DESTDIR
-
-gzip -c ${ARCHIVE_FILE} > $ARCHIVE_FILE.$TODAY.gz
-mv $ARCHIVE_FILE.$TODAY.gz $DESTDIR
-
-gzip -c ${PA_FILE} > $PA_FILE.$TODAY.gz
-mv $PA_FILE.$TODAY.gz $DESTDIR
+     rm -f ${TMPDIR}/${f}
+  else
+     logger -m "WEEWX_BACKUP - cannot find db file ${ARCHIVE_DIR}/${f}"
+  fi
+done
 
 logger "WEEWX_BACKUP - complete to $DESTDIR"
-
-# we leave it here and grab it manually from another system
-
-rm -f "${TMPDIR}"/$ARCHIVE_FILE.$TODAY.gz 
-rm -f "${TMPDIR}"/$ARCHIVE_FILE
-
-rm -f "${TMPDIR}"/$MEM_FILE.$TODAY.gz 
-rm -f "${TMPDIR}"/$MEM_FILE
-
-rm -f "${TMPDIR}"/$PA_FILE.$TODAY.gz 
-rm -f "${TMPDIR}"/$PA_FILE
 
